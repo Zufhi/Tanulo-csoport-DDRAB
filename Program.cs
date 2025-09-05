ef46034666c28d8c0defbc8bd1ae6fee1bb891c9
dewydd <117913498+Dewyydd@users.noreply.github.com>
2025-09-05 10:45:36 +0200

    class create

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Létra;
+using System.IO;
+
+StreamReader sr = new StreamReader("letra.txt");
+string[] temp = sr.ReadLine().Split(", ");
+int[] line = new int[temp.Length];
+
+for (int i = 0; i < line.Length; i++)
+{ 
+    line[i] = int.Parse(temp[i]);
+}
+
+Dobas D = new Dobas(line);
