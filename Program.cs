66453ee08ab66febabb938e8d9183d236f6e24b0
dewydd <117913498+Dewyydd@users.noreply.github.com>
2025-09-05 16:12:41 +0200

    kész cucc

--- Program.cs
+++ Program.cs
@@ -6,8 +6,36 @@ string[] temp = sr.ReadLine().Split(", ");
 int[] line = new int[temp.Length];
 
 for (int i = 0; i < line.Length; i++)
-{ 
+{
     line[i] = int.Parse(temp[i]);
 }
 
 Dobas D = new Dobas(line);
+
+int jelenlegi = 0;
+int vissza_count = 0;
+Console.WriteLine("2.feladat");
+foreach (var item in D.DobasElemek)
+{
+    jelenlegi += item;
+    if (jelenlegi % 10 == 0)
+    {
+        jelenlegi -= 3;
+        vissza_count++;
+    }
+
+    Console.Write($"{jelenlegi} ");
+}
+Console.WriteLine($"\n3. feladat\nA játék során {vissza_count} alkalommal lépett létrára.");
+
+Console.WriteLine("4.feladat");
+if (jelenlegi > 45)
+{
+    Console.WriteLine("A játékot befejezte.");
+}
+else
+{
+    Console.WriteLine("A játékot abbahagyta.");
+}
+
+Console.ReadKey();
