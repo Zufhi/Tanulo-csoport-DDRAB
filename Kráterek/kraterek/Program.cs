d00b884c74304ecd2dcf26a27091f70a5eadc58d
dewydd <117913498+Dewyydd@users.noreply.github.com>
2025-09-11 11:28:59 +0200

    mappa

---
+++ Kráterek/kraterek/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace kraterek
+{
+    class Program
+    {
+        static double tavolsag(double x1, double y1, double x2, double y2)
+        {
+            return Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)); ;
+        }
+        static void Main(string[] args)
+        {
+            List<krater> Adatok = new List<krater>();
+            foreach (var sor in File.ReadAllLines("felszin_tvesszo.txt")) 
+            {
+                Adatok.Add(new krater(sor));
+            }
+
+            Console.WriteLine("2. feladat");
+            int darab = 0;
+            foreach (var a in Adatok) 
+            {
+                darab++;
+            }
+            Console.WriteLine("A kráterek száma: " + darab);
+
+
+            Console.WriteLine("3. feladat");
+            Console.Write("Kérem egy kráter nevét: ");
+            string input = Console.ReadLine();
+            foreach (var a in Adatok) 
+            {
+                if (a.nev == input) 
+                {
+                    Console.WriteLine($"A(z) {a.nev} középpontja X={a.Xkordinata} Y={a.Ykordinata} sugara R={a.sugar}.");
+                }
+            }
+
+            Console.WriteLine("4. feladat");
+            double max = Adatok[0].sugar;
+            string maxNev = Adatok[0].nev;
+
+            foreach (var a in Adatok) 
+            {
+                if (a.sugar > max) 
+                {
+                    max = a.sugar;
+                    maxNev = a.nev;
+                }
+            }
+
+            Console.WriteLine($"A legnagyobb kráter neve és sugara: {maxNev} {max}");
+
+
+            Console.WriteLine("6. feladat");
+            Console.Write("Kérem egy kráter nevét: ");
+            string input2 = Console.ReadLine();
+            List<string> NincsKozosResz = new List<string>();
+            krater BekertKrater = null;
+
+            foreach (var a in Adatok)
+            {
+                if (a.nev == input2) 
+                {
+                    BekertKrater = a;
+                    break;
+                }
+            }
+
+            foreach (var a in Adatok) 
+            {
+                if (a.nev != BekertKrater.nev) 
+                {
+                    if (tavolsag(a.Xkordinata, a.Ykordinata, BekertKrater.Xkordinata, BekertKrater.Ykordinata) > (a.sugar + BekertKrater.sugar)) 
+                    {
+                        NincsKozosResz.Add(a.nev);
+                    }
+                }
+            }
+
+            Console.Write("Nincs közös része: ");
+            foreach (var n in NincsKozosResz) 
+            {
+                Console.Write($" {n},");
+            }
+
+            Console.WriteLine("7. feladat");
+
+            foreach (var a in Adatok) 
+            {
+                foreach (var b in Adatok) 
+                {
+                    if (a.sugar > b.sugar) 
+                    {
+                        if (tavolsag(a.Xkordinata, a.Ykordinata, b.Xkordinata, b.Ykordinata) < (a.sugar - b.sugar)) 
+                        {
+                            Console.WriteLine($"A(z) {a.nev} kráter tartalmazza a(z) {b.nev} krátert.");
+                        }
+                    }
+                }
+            }
+
+            StreamWriter sw = new StreamWriter("terulet.txt");
+
+            foreach (var a in Adatok) 
+            {
+                sw.WriteLine($"{Math.Round(((a.sugar * a.sugar) * Math.PI), 2)}\t{a.nev}");
+            }
+
+            sw.Close();
+        }
+    }
+}
---
+++ Könyvkiadás/konyvek/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.CodeDom.Compiler;
+
+namespace konyvek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<konyvek> list = new List<konyvek>();
+            foreach (var s in File.ReadLines("kiadas.txt"))
+            {
+                konyvek sor = new konyvek(s);
+                list.Add(sor);
+            }
+            //2. feladat
+            Console.Write("2. feladat:\nSzerző: ");
+            string Szerző = Console.ReadLine();
+            int db = 0;
+            foreach (var item in list)
+            {
+                if (item.Leiras.Contains(Szerző))
+                {
+                    db++;
+                }
+            }
+            if (db == 0)
+            {
+                Console.WriteLine("Nem adtak ki");
+            }
+            else
+            {
+                Console.WriteLine($"{db} könyvkiadás");
+            }
+            //3. feladat
+            db = 0;
+            Console.Write("3. feladat:\nLegnagyobb példányszám: ");
+            int m_db = 0;
+            foreach (var item in list)
+            {
+                if (db < item.Peldany)
+                {
+                    db = item.Peldany;
+                }
+            }
+            foreach (var item in list)
+            {
+                if (db == item.Peldany)
+                {
+                    m_db++;
+                }
+            }
+            Console.WriteLine($"{db}, előfordult {m_db} alkalommal");
+            //4. feladat
+            Console.WriteLine("4. feladat:");
+            foreach (var item in list)
+            {
+                if (item.Eredete == "kf" && item.Peldany >= 40000)
+                {
+                    Console.WriteLine($"{item.Ev}/{item.NE}. {item.Leiras}");
+                    break;
+                }
+            }
+            //5. feladat
+            Console.WriteLine("5. feladat:");
+            StreamWriter html = new StreamWriter("tabla.html");
+            html.WriteLine("<table>\r\n<tr><th>Év</th><th>Magyar kiadás</th><th>Magyar példányszám</th><th>Külföldi\r\nkiadás</th><th>Külföldi példányszám</th></tr>");
+            Console.WriteLine("Év\tMagyar kiadás\tMagyar példányszám\tKülföldi kiadás\tKülföldi példányszám");
+            List<int> ints = new List<int>();
+            int eev = 0;
+            foreach (var item in list)
+            {
+                int ev = 0;
+                int mk = 0;
+                int mp = 0;
+                int kk = 0;
+                int kp = 0;
+                foreach (var s in list)
+                {
+                    if (!ints.Contains(item.Ev))
+                    {
+                        eev = s.Ev;
+                        ev = item.Ev;
+                        
+                        if (s.Ev == item.Ev)
+                        {
+                            if (s.Eredete == "ma")
+                            {
+                                mk++;
+                                mp += s.Peldany;
+                            }
+                            else
+                            {
+                                kk++;
+                                kp += s.Peldany;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        
+
+                        break;
+                    }
+
+
+                }
+                ints.Add(ev);
+                if (ev != 0) {
+                    Console.WriteLine($"{ev}\t\t{mk}\t\t{mp}\t\t{kk}\t\t{kp}");
+                    html.WriteLine($"<tr><td>{ev}</td><td>{mk}</td><td>{mp}</td><td>{kk}</td><td>{kp}</td></tr>");
+                }
+            }
+            html.WriteLine("</table>");
+            html.Close();
+
+            //6. feladat
+            Console.WriteLine("6. feladat:\nLegalább kétszer, nagyobb példányszámban újra kiadott könyvek:");
+            List<string> cimre = new List<string>();
+            foreach (var item in list)
+            {
+                if (!cimre.Contains(item.Leiras))
+                {
+                    cimre.Add(item.Leiras);
+                }
+            }
+            foreach (var item in cimre)
+            {
+                int kiadas = 0;
+                int p1 = 0;
+
+                foreach (var s in list)
+                {
+
+                    if (item == s.Leiras)
+                    {
+                        if (p1 == 0)
+                        {
+                            p1 = s.Peldany;
+                        } 
+                        else if (p1 < s.Peldany)
+                        {
+                            kiadas++;
+                        }
+                        
+                    }
+                }
+                if (kiadas >= 2) {
+                    Console.WriteLine($"{item}");
+                }
+            }
+        }
+    }
+}
